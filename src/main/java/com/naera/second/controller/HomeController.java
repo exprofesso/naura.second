bbf6f5d43727c8c278cb16e16578560b5087b349
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-12 22:58:16 +0300

    first task

--- src/main/java/com/naera/second/controller/HomeController.java
+++ src/main/java/com/naera/second/controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.naera.second.controller;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Controller
-public class HomeController {
+
+//@EnableWebMvc
+//@Configuration
+public class HomeController implements WebMvcConfigurer {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello world";
+        return "hello";
     }
 
     @GetMapping("/home")
@@ -16,9 +27,20 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/name")
-    public String name(){
-        return "name";
-    }
 
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("hello");
+//    }
+//
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//
+//        bean.setViewClass(JstlView.class);
+//        bean.setPrefix("/WEB-INF/view/");
+//        bean.setSuffix(".jsp");
+//
+//        return bean;
+//    }
 }
