00fea4ef2db0c7bb5ec3c95e4c025c27c6393731
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-13 23:45:26 +0300

    first task login.html + WebSecurityConfig

--- src/main/java/com/naera/second/controller/HomeController.java
+++ src/main/java/com/naera/second/controller/HomeController.java
@@ -1,15 +1,12 @@
 package com.naera.second.controller;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+
 
 //@Controller
 
@@ -33,7 +30,7 @@ public class HomeController implements WebMvcConfigurer {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
-   //     registry.addViewController("/login").setViewName("login");
+       registry.addViewController("/login").setViewName("login");
     }
 
 }
