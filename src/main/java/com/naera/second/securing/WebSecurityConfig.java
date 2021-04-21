228a39632d3127b845034957782015c54d24fca5
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-21 23:59:09 +0300

    version 3.1

--- src/main/java/com/naera/second/securing/WebSecurityConfig.java
+++ src/main/java/com/naera/second/securing/WebSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
