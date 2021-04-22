0f12b7d4f1abe5855e072518a2f7be92fc2ea0ad
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-22 22:56:25 +0300

    version 3.1

--- src/main/java/com/naera/second/securing/WebSecurityConfig.java
+++ src/main/java/com/naera/second/securing/WebSecurityConfig.java
@@ -1,18 +1,38 @@
 package com.naera.second.securing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+                .withUser(User.withUsername("user").password("password").roles("USER"))
+                .withUser(User.withUsername("admin").password("password").roles("ADMIN"));
+    }
 
 
 
@@ -20,7 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/hello", "/users", "/registration").permitAll()
+                .antMatchers("/", "/hello","/registration").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
