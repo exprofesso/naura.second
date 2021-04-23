bced3dcc8252122655af900c74d55359a0c5e2a4
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-23 23:34:32 +0300

    version 3.2

---
+++ src/main/java/com/naera/second/controller/UserController.java
@@ -0,0 +1,106 @@
+package com.naera.second.controller;
+
+import com.naera.second.model.User;
+import com.naera.second.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    //    private final UserService userService;
+//
+//    @Autowired
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    // добавить пользователя
+//    @PostMapping(value = "/registration")
+//    public ResponseEntity<?> create(@RequestBody User user) {
+//        userService.create(user);
+//        return new ResponseEntity<>(HttpStatus.CREATED);
+//    }
+//
+//    // вывести всех
+//    @GetMapping(value = "/users")
+//    public ResponseEntity<List<User>> read() {
+//        final List<User> users = userService.readAll();
+//        return users != null && !users.isEmpty()
+//                ? new ResponseEntity<>(users, HttpStatus.OK)
+//                : new ResponseEntity<>(users, HttpStatus.NOT_FOUND);
+//    }
+//
+//    // вывести одного
+//    @GetMapping(value = "/users/{id}")
+//    public ResponseEntity<User> read(@PathVariable(name = "id") int id) {
+//        final User user = userService.read(id);
+//        return user != null
+//                ? new ResponseEntity<>(user, HttpStatus.OK)
+//                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+//
+//    // Update
+//    @PutMapping(value = "/users/{id}")
+//    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody User user) {
+//        final boolean updated = userService.update(user, id);
+//
+//        return updated
+//                ? new ResponseEntity<>(HttpStatus.OK)
+//                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+//    }
+//
+//    // Удаление
+//    @DeleteMapping(value = "/users/{id}")
+//    public ResponseEntity<?> delete(@PathVariable(name = "id") int id) {
+//        final boolean deleted = userService.delete(id);
+//
+//        return deleted
+//                ? new ResponseEntity<>(HttpStatus.OK)
+//                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+//    }
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Optional<User> getAllUsers(@PathVariable Integer id) {
+        return userService.getById(id);
+    }
+
+    @RequestMapping(value = "/users/{name}", method = RequestMethod.GET)
+    public List<User> getUserByName(@PathVariable String name) {
+        return userService.findByName(name);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> getAll() {
+        return userService.getAllUser();
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+    public HttpStatus deleteUsers(@PathVariable Integer id) {
+        userService.deleteUser(id);
+        return HttpStatus.NO_CONTENT;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public HttpStatus insertUsers(@RequestBody User user) {
+        return userService.addUser(user) ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST;
+    }
+
+    // пока не знаю как
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "Login";
+    }
+
+
+    @RequestMapping(value = "/users", method = RequestMethod.PUT)
+    public HttpStatus updateUsers(@RequestBody User user) {
+        return userService.updateUser(user) ? HttpStatus.ACCEPTED : HttpStatus.BAD_REQUEST;
+    }
+}
